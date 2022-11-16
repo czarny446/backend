dabcf95c1dfb90d51dedcf6ab0fa6700a6c1b8f1
czarny446 <114070245+czarny446@users.noreply.github.com>
2022-11-16 11:32:57 +0100

    Add files via upload

---
+++ osadnicy/zaloguj.php
@@ -0,0 +1,32 @@
+<?php
+
+ require_once "connect.php";
+
+ $polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
+ 
+ if($polaczenie->connect_errno!=0)
+ {
+	 echo "Error:".$polaczenie->connect_errno;
+ }
+ else
+ {
+	 $login=$_POST['login'];
+     $haslo=$_POST['haslo']; 
+	 
+	 echo "it works";
+	 
+	 $polaczenie->close();
+ } 
+ 
+
+
+ //echo $login."<br/>";
+ //echo $haslo."<br/>";
+
+
+
+
+
+
+
+?>
